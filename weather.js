62fbda037ce12336d491bd7574ac6122a940b102
krishna kushwaha <krishnakushwaha2248@gmail.com>
2025-07-04 19:05:38 +0530

    Adding Logic of JavaScript

--- weather.js
+++ weather.js
@@ -1,12 +1,41 @@
-const url = " https://api.weatherapi.com/v1/current.json?key=485c1438aead4ac1894132431250307&q=Kanpur";
+const url =
+  "https://api.weatherapi.com/v1/current.json?key=485c1438aead4ac1894132431250307&q=";
 
-let weather = async() => {
-    const response = await fetch(url);
-    const data = await response.json();
- 
-    console.log(data.location);
-    console.log(data.current);
-    
-    
-}
-//weather();
+const city = document.querySelector("#input");
+const temp = document.querySelector("#temp");
+const cityName = document.querySelector("#location p");
+const msg = document.querySelector("#temptext p");
+const icon = document.querySelector("#temptext img");
+const humidity = document.querySelector("#humidity span");
+const feelslike = document.querySelector("#feelslike span");
+const wind = document.querySelector("#wind span");
+const visibility = document.querySelector("#visibility span");
+const uv = document.querySelector("#uv span");
+const pressure = document.querySelector("#pressure span");
+
+let weather = async (city) => {
+  const newUrl = `${url}${city}`;
+  const response = await fetch(newUrl);
+  const data = await response.json();
+
+  console.log(data.location);
+  console.log(data.current);
+  cityName.innerText = `${data.location.name} ${data.location.region},${data.location.country}`;
+  temp.innerText = `${data.current.temp_c}`;
+  msg.innerText = data.current.condition["text"];
+  icon.src = data.current.condition["icon"];
+  humidity.innerText = data.current.humidity;
+  feelslike.innerText = data.current.feelslike_c;
+  wind.innerText = data.current.wind_kph;
+  visibility.innerText = data.current.vis_km;
+  uv.innerText = data.current.uv;
+  pressure.innerText = data.current.pressure_mb;
+};
+
+city.addEventListener("change", () => {
+  weather(city.value);
+});
+
+window.addEventListener("load", () => {
+  weather("kanpur");
+});
